ab0558d01c35a43013d9ce1cd59f39a14c6a6e45
vboxuser <vboxuser@ArqManuLinux.myguest.virtualbox.org>
2024-06-08 00:45:25 -0300

    corrigiendo TP3

--- TP3/comenhamburguesas.c
+++ TP3/comenhamburguesas.c
@@ -4,59 +4,49 @@
 #define NUMBER_OF_THREADS 2
 #define CANTIDAD_INICIAL_HAMBURGUESAS 20
 int cantidad_restante_hamburguesas = CANTIDAD_INICIAL_HAMBURGUESAS;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 int turno = 0;
 
 void *comer_hamburguesa(void *tid)
 {
-    long int thread_id = (long int)tid;
-    while (1 == 1)
-    {
-        pthread_mutex_lock(&mutex);
-        while (turno != (int)thread_id) 
-        {
-            pthread_cond_wait(&cond, &mutex);
-        }
-if (cantidad_restante_hamburguesas > 0)
-{
-    printf("Hilo(comensal) %ld se come una hamburguesa. Quedan %d hamburguesas.\n", thread_id, cantidad_restante_hamburguesas);
-    cantidad_restante_hamburguesas--;
-    turno = (turno + 1) % NUMBER_OF_THREADS;
-    pthread_cond_signal(&cond); 
-} 
-    else 
-    {
-        printf("SE TERMINARON LAS HAMBURGUESAS :(\n");
-        
-        pthread_mutex_unlock(&mutex);
-        pthread_exit(NULL);
-    }
-    pthread_mutex_unlock(&mutex);
-    } 
-    return NULL;
+	while (1 == 1)
+	{ 
+		while(turno!=(int)tid);
+    // INICIO DE LA ZONA CRÍTICA
+		if (cantidad_restante_hamburguesas > 0)
+		{
+			printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n", (int) tid, cantidad_restante_hamburguesas);
+			cantidad_restante_hamburguesas--; // me como una hamburguesa
+		}
+		else
+		{
+			printf("SE TERMINARON LAS HAMBURGUESAS :( \n");
+            turno = (turno + 1)% NUMBER_OF_THREADS;
+			pthread_exit(NULL); // forzar terminacion del hilo
+		}
+    // SALIDA DE LA ZONA CRÍTICA   
+        turno = (turno + 1)% NUMBER_OF_THREADS;
+	}
 }
-int main(int argc, char *argv[])
-{ 
-    pthread_t threads[NUMBER_OF_THREADS];
-    int status, i, ret;
-    for (int i = 0; i < NUMBER_OF_THREADS; i++)
-    { 
-        printf("Hola!, soy el hilo principal. Estoy creando el hilo %d \n", i);
-        status = pthread_create(&threads[i], NULL, comer_hamburguesa, (void *)(long int)i);
-        if (status != 0) 
-        {
-            printf("Algo salio mal, al crear el hilo recibi el codigo de error %d \n", status);
-            exit(-1);
-        }
-    }
 
-    for (i = 0; i < NUMBER_OF_THREADS; i++) 
-    {
-        void *retval;
-        ret = pthread_join(threads[i], &retval); 
-    }
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
-    pthread_exit(NULL); 
-} 
+int main(int argc, char *argv[])
+{
+	pthread_t threads[NUMBER_OF_THREADS];
+	int status, i, ret;
+	for (int i = 0; i < NUMBER_OF_THREADS; i++)
+	{
+		printf("Hola!, soy el hilo principal. Estoy creando el hilo %d \n", i);
+		status = pthread_create(&threads[i], NULL, comer_hamburguesa, (void *)i);
+		if (status != 0)
+		{
+			printf("Algo salio mal, al crear el hilo recibi el codigo de error %d \n", status);
+			exit(-1);
+		}
+	}
+
+	for (i = 0; i < NUMBER_OF_THREADS; i++)
+	{
+		void *retval;
+		ret = pthread_join(threads[i], &retval); // espero por la terminacion de los hilos que cree
+	}
+	pthread_exit(NULL); // como los hilos que cree ya terminaron de ejecutarse, termino yo tambien.
+}
